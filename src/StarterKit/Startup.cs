b21c9062c20c14297ee677dd63be3a10109e3c29
Jose Carlo San Pedro <josecarlosanpedro0302@gmail.com>
2018-12-26 19:04:53 +0800

    remove comments in startuo

--- src/StarterKit/Startup.cs
+++ src/StarterKit/Startup.cs
@@ -13,26 +13,13 @@ namespace StaterKit
 {
     public class Startup
     {
-        // public static IConfigurationRoot Configuration;
-
-        // public Startup(IHostingEnvironment env) 
-        // {
-        //     var builder = new ConfigurationBuilder()
-        //         .SetBasePath(env.ContentRootPath)
-        //         .AddIniFile("appSettings.json", optional:false, reloadOnChange:true);
-        //     Configuration = builder.Build();
-        // } 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            // loggerFactory.AddConsole();
             loggerFactory.AddConsole();
             loggerFactory.AddDebug();
             if (env.IsDevelopment())
@@ -41,14 +28,8 @@ namespace StaterKit
             } else {
                 app.UseExceptionHandler();
             }
-            app.UseStatusCodePages(); // to show the result in the browser
+            app.UseStatusCodePages();
             app.UseMvc();
-
-            // app.Run(async (context) =>
-            // {
-            //     await context.Response.WriteAsync("Hello World!");
-            //     // throw new Exception("Example exception");
-            // });
         }
     }
 }
